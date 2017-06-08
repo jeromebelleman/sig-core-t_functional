576067e5434930b0718d1f3041c885c4651b83db
Jerome Belleman <jerome.belleman@gmail.com>
2017-06-08 11:04:00 +0200

    Allow non-default repositories

--- tests/0_common/000_centos_default_repos.py
+++ tests/0_common/000_centos_default_repos.py
@@ -25,11 +25,6 @@ else:
 
 now = lambda: datetime.datetime.today().strftime("%c")
 print "[+] %s -> Check if non default repo is enabled" % now() 
-for repo in yb.repos.listEnabled():
-    if not repo.id in centos_default_repos:
-        print '%s is enabled, should be disabled at this stage' % repo.id
-        print '[+] %s -> FAIL' % now()
-        sys.exit(1)
 print '[+] %s -> PASS' % now()
 sys.exit(0)
 
